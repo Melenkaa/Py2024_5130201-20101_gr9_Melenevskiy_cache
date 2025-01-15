43f9653af0f3c5c3db9b87b295d111b4ef1fc724
Melenkaa <66130592+Melenkaa@users.noreply.github.com>
2025-01-15 22:45:47 +0300

    Add files via upload

---
+++ wrapper.py
@@ -0,0 +1,37 @@
+from functools import *
+from threading import *
+
+class Cache:
+    def __init__(self, depth):
+        self.depth = depth
+        self.cache = {}
+        self.lock = threading.Lock()
+
+    def get(self, key):
+        with self.lock:
+            return self.cache.get(key)
+
+    def set(self, key, value):
+        with self.lock:
+            if len(self.cache) >= self.depth:
+                self.cache.pop(next(iter(self.cache)))
+            self.cache[key] = value
+
+def memorize(depth):
+    def decorator(fn):
+        cache = Cache(depth)
+
+        @functools.wraps(fn)
+        def wrapper(*args):
+            key = (fn.__name__,) + args
+            
+            cachedResult = cache.get(key)
+            if cachedResult is not None:
+                return cachedResult
+
+            result = fn(*args)
+            cache.set(key, result)
+            return result
+
+        return wrapper
+    return decorator
